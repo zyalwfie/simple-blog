f379ad96231d85326b660b0661df4425e5515f2c
Ziyad Alwafie <zyalwfie@gmail.com>
2025-07-11 17:57:01 +0800

    blog-system(frontend): #15 udpate ui single blog page using flowbite

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,5 +1,5 @@
 <x-layout :title="$title">
-    <article class="py-8">
+    {{-- <article class="py-8">
         <h2 class="text-2xl font-bold text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
             <a href="#">{{ $post->author->name }}</a> | 1 January 2025
@@ -8,5 +8,39 @@
         <a href="/posts"
             class="block max-w-max px-4 py-2 rounded-md bg-indigo-600 text-white hover:bg-indigo-800">
             &laquo; Back</a>
-    </article>
+    </article> --}}
+
+    <!--
+Install the "flowbite-typography" NPM package to apply styles and format the article content:
+
+URL: https://flowbite.com/docs/components/typography/
+-->
+
+    <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
+        <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
+            <article
+                class="mx-auto w-full max-w-4xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+                <a href="/posts" class="px-4 py-2 rounded-md bg-indigo-600 hover:bg-indigo-800 text-white no-underline">Back to all posts</a>
+                <header class="my-4 lg:mb-6 not-format">
+                    <address class="flex items-center mb-6 not-italic">
+                        <div class="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                            <img class="mr-4 w-16 h-16 rounded-full"
+                                src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Jese Leos">
+                            <div>
+                                <a href="#" rel="author"
+                                    class="text-xl font-bold text-gray-900 dark:text-white">{{ $post->author->name }}</a>
+                                <p class="text-base text-gray-500 dark:text-gray-400">{{ $post->category->name }}</p>
+                                <p class="text-base text-gray-500 dark:text-gray-400"><time pubdate
+                                        datetime="2022-02-08" title="February 8th, 2022">{{ $post->created_at->diffForHumans() }}</time></p>
+                            </div>
+                        </div>
+                    </address>
+                    <h1
+                        class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+                        {{ $post->title }}</h1>
+                </header>
+                <p>{{ $post->body }}</p>
+            </article>
+        </div>
+    </main>
 </x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,19 +1,4 @@
 <x-layout :title="$title">
-    {{-- <section class="grid grid-cols-1 gap-x-4">
-        @foreach ($posts as $post)
-            <article class="border-b border-gray-500 py-8">
-                <h2 class="text-2xl font-bold text-gray-900">{{ $post['title'] }}</h2>
-                <div class="text-base text-gray-500">
-                    <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | 1 January 2025 | <a href="/categories/{{ $post->category->slug }}" class="hover:underline">{{ $post->category->name }}</a>
-                </div>
-                <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
-                <a href="/posts/{{ $post['slug'] }}"
-                    class="block max-w-max px-4 py-2 rounded-md bg-indigo-600 text-white hover:bg-indigo-800">Read more
-                    &raquo;</a>
-            </article>
-        @endforeach
-    </section> --}}
-
     <div class="py-8 mx-auto max-w-screen-xl">
         <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
             @foreach ($posts as $post)
